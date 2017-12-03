f2ed4bba661c420ce9290b020eb6df5a745398c7
Евгений Татаринцев <evtatarintsev@ya.ru>
2017-12-03 21:46:03 +0300

    fix

--- dispensary/urls.py
+++ dispensary/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+
+admin.site.site_title = 'Кировский спортивный диспансер'
+admin.site.site_header = 'Кировский спортивный диспансер'
+
+
 urlpatterns = [
     url(r'^', admin.site.urls),
 ]
